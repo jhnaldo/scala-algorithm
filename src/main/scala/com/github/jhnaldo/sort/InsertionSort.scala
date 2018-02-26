9d87281ff0be4bab9399d58e3ab9950e2ff7a296
jhnaldo <jhpjhp0223@gmail.com>
2018-02-27 00:09:20 +0900

    [Sort] Using tail-call in insertion sort

--- src/main/scala/com/github/jhnaldo/sort/InsertionSort.scala
+++ src/main/scala/com/github/jhnaldo/sort/InsertionSort.scala
@@ -2,20 +2,20 @@ package com.github.jhnaldo.sort
 
 object InsertionSort {
   /**
-   * Insertion sorting algorithm
+   * Sorts a list of elements using insertion sort algorithm
    *
    * @param list a list of n elements
    * @return the sorted list of n elements
    */
   def sort[A <% Ordered[A]](list: List[A]): List[A] = {
     def insertList(list: List[A], sorted: List[A]): List[A] = list match {
-      case hd :: tl => insertList(tl, insertElem(hd, sorted))
+      case hd :: tl => insertList(tl, insertElem(Nil, hd, sorted))
       case Nil => sorted
     }
 
-    def insertElem(elem: A, sorted: List[A]): List[A] = sorted match {
-      case hd :: tl if elem > hd => hd :: insertElem(elem, tl)
-      case _ => elem :: sorted
+    def insertElem(pre: List[A], elem: A, sorted: List[A]): List[A] = sorted match {
+      case hd :: tl if elem > hd => insertElem(hd :: pre, elem, tl)
+      case _ => pre.reverse ++ (elem :: sorted)
     }
 
     insertList(list, Nil)
